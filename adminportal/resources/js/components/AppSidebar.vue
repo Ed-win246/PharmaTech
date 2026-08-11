b892835d72b4f8f7eb84cb5c764d47bbae774c25
Ed-win246 <edwinatuhaire96@gmai.com>
2026-08-11 14:57:47 +0300

    adding sales report on the side bar

--- adminportal/resources/js/components/AppSidebar.vue
+++ adminportal/resources/js/components/AppSidebar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { Link } from '@inertiajs/vue3';
-import {  LayoutGrid , Settings} from '@lucide/vue';
+import {  LayoutGrid , PieChart, Settings} from '@lucide/vue';
 import AppLogo from '@/components/AppLogo.vue';
 import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
@@ -23,14 +23,15 @@ const mainNavItems: NavItem[] = [
         href: dashboard(),
         icon: LayoutGrid,
     },
+    {
+        title: 'Sales Reports',
+        href: 'sales-reports',
+        icon: PieChart,
+    },
 ];
 
 const footerNavItems: NavItem[] = [
-    // {
-    //     title: 'Repository',
-    //     href: 'https://github.com/laravel/vue-starter-kit',
-    //     icon: FolderGit2,
-    // },
+
     {
         title: 'Settings',
         href: 'https://laravel.com/docs/starter-kits#vue',
